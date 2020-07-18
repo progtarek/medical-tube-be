a2d93b058b299f6dd51b798ac409d490a3959559
progtarek <progtarek@gmail.com>
2020-07-18 23:55:37 +0200

    add admin guard roles

---
+++ src/core/guards/role.guard.ts
@@ -0,0 +1,21 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    if (user.role === 'ADMIN') {
+      return true;
+    }
+    return false;
+  }
+}
--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -9,8 +9,10 @@ import { CreateUserDto } from 'src/modules/auth/dto/create-user.dto';
 import { User } from 'src/db/schemas/user.schema';
 import { UsersService } from '../services/users.service';
 import { AuthGuard } from '@nestjs/passport';
+import { RoleGuard } from 'src/core/guards/role.guard';
 
 @Controller('users')
+@UseGuards(new RoleGuard())
 @UseGuards(AuthGuard())
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
